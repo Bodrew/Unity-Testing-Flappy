c53f5c20454ccad824edb078eaec402ccaa0b8bd
Andrew Bowen <bodrew12@gmail.com>
2024-05-04 13:08:40 -0400

    Added empty pipe destroy script

--- Assets/PipeDestroyerScript.cs
+++ Assets/PipeDestroyerScript.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeScript : MonoBehaviour
+public class PipeDestroyerScript : MonoBehaviour
 {
-    public float moveSpeed = 200;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +13,6 @@ public class PipeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        
     }
 }
